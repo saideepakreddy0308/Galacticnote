880ce0a4f171d27612c2a02aa3be4bc1a1f19bb3
Deepak <saideepakreddy0308@gmail.com>
2022-04-02 16:05:29 +0530

    data validation using express-validator

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary&directConnection=true&ssl=false"
+const mongoURI = "mongodb://localhost:27017/cloudnotes?readPreference=primary&directConnection=true&ssl=false"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
