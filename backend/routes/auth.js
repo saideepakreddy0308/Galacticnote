346336e7671857f139596cd3adf586b38d612745
Deepak <saideepakreddy0308@gmail.com>
2022-04-13 09:59:46 +0530

    added clientside

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -13,6 +13,7 @@ router.post('/createuser', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
+   let success = false;
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -66,12 +67,14 @@ router.post('/login', [
     try {
       let user = await User.findOne({email});
       if(!user){
+        success = false
         return res.status(400).json({error: "Please try to login with correct credentials"});
       }
   
       const passwordCompare = await bcrypt.compare(password, user.password);
       if(!passwordCompare){
-        return res.status(400).json({error: "Please try to login with correct credentials"});
+        success = false
+        return res.status(400).json({ success, error: "Please try to login with correct credentials" });
       }
   
       const data = {
@@ -80,7 +83,8 @@ router.post('/login', [
         }
       }
       const authtoken = jwt.sign(data, JWT_SECRET);
-      res.json({authtoken})
+      success = true;
+      res.json({ success, authtoken })
   
     } catch (error) {
       console.error(error.message);
