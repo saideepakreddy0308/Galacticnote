fbdc082f900a264c3306c6bc2855a8a85dacd5a1
Deepak <saideepakreddy0308@gmail.com>
2022-04-13 12:21:07 +0530

    updated  & fetchAllNotes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const connectToMongo = require('./db');
 const express = require('express')
+var cors = require('cors') 
 
 connectToMongo();
 const app = express()
 const port = 5000
+
+app.use(cors())
 app.use(express.json())
 
 // Available Routes
@@ -11,5 +14,5 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`cloudnotes-backend listening at http://localhost:${port}`)
 })
