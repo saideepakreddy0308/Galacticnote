266a6e56506266a9cacb85666be90f3451d1abdc
Deepak <saideepakreddy0308@gmail.com>
2022-04-13 14:29:33 +0530

    adding alerts to components

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import { useHistory } from 'react-router-dom'
 
-const Signup = () => {
+const Signup = (props) => {
     const [credentials, setCredentials] = useState({name:"",email: "", password: "",cpassword: ""}) 
     let history = useHistory();
     const handleSubmit = async (e) => {
@@ -16,9 +16,15 @@ const Signup = () => {
         });
         const json = await response.json()
         console.log(json);
+        if (json.success){
             // Save the auth token and redirect
             localStorage.setItem('token', json.authtoken);
             history.push("/"); 
+            props.showAlert("Account Created Successfully","success")
+        }
+        else{
+            props.showAlert("Invalid Credentials","danger")
+        }
     }
 
     const onChange = (e) => {
